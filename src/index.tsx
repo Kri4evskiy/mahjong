ef38b45220b79e68a77cf6855341f5787f59cd73
Kri4evskiy <kri4evskiy@gmail.com>
2022-07-27 09:46:16 +0300

    [feat]: making ci/cd

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Layout } from './components/Layout'
 import { Playground } from './components/Playground'
-import reportWebVitals from './reportWebVitals'
 
 const App: React.FC = () => {
   return (
@@ -16,8 +15,3 @@ const App: React.FC = () => {
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<App />)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
